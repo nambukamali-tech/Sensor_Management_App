f3fa0598af950baf0234512daff5889a0134b2d9
Nambu-Kamali <nambu.kamali@primemoversolutions.in>
2026-05-13 15:31:09 +0530

    Initial ASP .NET core Project Setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Snape_Lite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
